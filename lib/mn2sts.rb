42b1670b2af785f1a1f88efefe19d88c2c208a3f
Ronald Tse <ronald.tse@ribose.com>
2020-06-08 14:49:01 +0800

    Fix convert method

--- lib/mn2sts.rb
+++ lib/mn2sts.rb
@@ -16,13 +16,14 @@ module Mn2sts
     message.strip
   end
 
-  def self.convert(url_path, output_path, xsl_stylesheet)
-    return if url_path.nil? || output_path.nil? || xsl_stylesheet.nil?
+  def self.convert(xml_path_in, xml_path_out)
+    return if xml_path_in.nil? || xml_path_out.nil?
 
     puts MN2STS_JAR_PATH
-    cmd = ['java', '-Xss5m', '-Xmx1024m', '-jar', MN2STS_JAR_PATH, '--xml-file',
-           url_path, '--xsl-file', xsl_stylesheet, '--pdf-file',
-           output_path].join(' ')
+    cmd = ['java', '-Xss5m', '-Xmx1024m', '-jar', MN2STS_JAR_PATH,
+           ' --xml-file-in', xml_path_in,
+           ' --xml-file-out', xml_path_out
+          ].join(' ')
 
     puts cmd
     _, error_str, status = Open3.capture3(cmd)
