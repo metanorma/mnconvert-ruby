324d5bb14791c096cc0a31cb549670691b43f488
Alexande B <abobrikovich@gmail.com>
2021-02-22 21:12:37 +0300

    Add rubocop configuration + fix issues

--- lib/mn2sts.rb
+++ lib/mn2sts.rb
@@ -1,8 +1,8 @@
-require 'open3'
-require 'mn2sts/version'
+require "open3"
+require "mn2sts/version"
 
 module Mn2sts
-  MN2STS_JAR_PATH = File.join(File.dirname(__FILE__), '../bin/mn2sts.jar')
+  MN2STS_JAR_PATH = File.join(File.dirname(__FILE__), "../bin/mn2sts.jar")
 
   def self.jvm_options
     options = []
@@ -15,23 +15,22 @@ module Mn2sts
   end
 
   def self.help
-    cmd = ['java', *jvm_options, '-jar', MN2STS_JAR_PATH].join(' ')
-    message, error_str, status = Open3.capture3(cmd)
+    cmd = ["java", *jvm_options, "-jar", MN2STS_JAR_PATH].join(" ")
+    message, = Open3.capture3(cmd)
     message
   end
 
   def self.version
-    cmd = ['java', *jvm_options, '-jar', MN2STS_JAR_PATH, '-v'].join(' ')
-    message, error_str, status = Open3.capture3(cmd)
+    cmd = ["java", *jvm_options, "-jar", MN2STS_JAR_PATH, "-v"].join(" ")
+    message, = Open3.capture3(cmd)
     message.strip
   end
 
   def self.convert(xml_path_in, xml_path_out, opts = {})
     return if xml_path_in.nil? || xml_path_out.nil?
 
-    puts MN2STS_JAR_PATH
-    cmd = ['java', '-Xss5m', '-Xmx1024m', *jvm_options, '-jar', MN2STS_JAR_PATH,
-           '--xml-file-in', xml_path_in, '--xml-file-out', xml_path_out].join(' ')
+    cmd = ["java", "-Xss5m", "-Xmx1024m", *jvm_options, "-jar", MN2STS_JAR_PATH,
+           "--xml-file-in", xml_path_in, "--xml-file-out", xml_path_out].join(" ")
 
     cmd += " --output-format iso" if opts[:iso]
 
@@ -43,5 +42,4 @@ module Mn2sts
       raise error_str
     end
   end
-
 end
--- spec/mn2sts_spec.rb
+++ spec/mn2sts_spec.rb
@@ -1,17 +1,19 @@
-require 'tmpdir'
-require 'fileutils'
+require "tmpdir"
+require "fileutils"
 
 RSpec.describe Mn2sts do
-
-  it 'matches the version number of JAR' do
-    expect(Mn2sts::VERSION.split('.')[0..1].join('.')).to eq(Mn2sts.version)
+  it "matches the version number of JAR" do
+    expect(Mn2sts::VERSION.split(".")[0..1].join(".")).to eq(Mn2sts.version)
     expect(Mn2sts::MN2STS_JAR_VERSION).to eq(Mn2sts.version)
   end
 
-  it 'converts XML to STS' do
+  it "help not empty" do
+    expect(Mn2sts.help).not_to be_empty
+  end
 
+  it "converts XML to STS" do
     Dir.mktmpdir do |dir|
-      sts_path = File.join(dir, 'rice-en.cd.sts.xml')
+      sts_path = File.join(dir, "rice-en.cd.sts.xml")
 
       FileUtils.rm_f(sts_path)
       begin
@@ -24,13 +26,11 @@ RSpec.describe Mn2sts do
       expect(File.exist?(sts_path)).to be true
       expect(File.read(sts_path)).to include '<code language="ruby"'
     end
-
   end
 
-  it 'converts XML to ISO STS' do
-
+  it "converts XML to ISO STS" do
     Dir.mktmpdir do |dir|
-      sts_path = File.join(dir, 'rice-en.cd.isosts.xml')
+      sts_path = File.join(dir, "rice-en.cd.isosts.xml")
 
       FileUtils.rm_f(sts_path)
       begin
@@ -43,12 +43,10 @@ RSpec.describe Mn2sts do
       expect(File.exist?(sts_path)).to be true
       expect(File.read(sts_path)).to include '<preformat preformat-type="ruby">'
     end
-
   end
 
   let(:mn_xml) do
-    Pathname.new(File.dirname(__dir__)).
-    join('spec', 'fixtures', 'rice-en.cd.mn.xml').to_s
+    Pathname.new(File.dirname(__dir__))
+      .join("spec", "fixtures", "rice-en.cd.mn.xml").to_s
   end
-
 end
