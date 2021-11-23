9c07911c747ab2118347ebdd09253fb009a6b4f6
Alexande B <abobrikovich@gmail.com>
2021-11-23 15:48:07 +0100

    Move Dir.mktmpdir to config.around

--- spec/mnconvert_spec.rb
+++ spec/mnconvert_spec.rb
@@ -12,95 +12,83 @@ RSpec.describe MnConvert do
   end
 
   it "converts XML to STS" do
-    Dir.mktmpdir do |dir|
-      sts_path = File.join(dir, "rice-en.cd.sts.xml")
-      FileUtils.rm_f(sts_path)
-      MnConvert.convert(
-        mn_xml, sts_path,
-        { input_format: :metanorma, debug: true }
-      )
-      expect(File.exist?(sts_path)).to be true
-      expect(File.read(sts_path)).to include '<code language="ruby"'
-    end
+    sts_path = File.join(Dir.pwd, "rice-en.cd.sts.xml")
+    FileUtils.rm_f(sts_path)
+    MnConvert.convert(
+      mn_xml, sts_path,
+      { input_format: :metanorma, debug: true }
+    )
+    expect(File.exist?(sts_path)).to be true
+    expect(File.read(sts_path)).to include '<code language="ruby"'
   end
 
   it "converts XML to STS (autodetect)" do
-    Dir.mktmpdir do |dir|
-      out_path = File.join(dir, "rice-en.xml")
-      MnConvert.convert(mn_xml, out_path)
-      expect(File.exist?(out_path)).to be true
-      expect(File.read(out_path)).to include '<code language="ruby"'
-    end
+    out_path = File.join(Dir.pwd, "rice-en.xml")
+    MnConvert.convert(mn_xml, out_path)
+    expect(File.exist?(out_path)).to be true
+    expect(File.read(out_path)).to include '<code language="ruby"'
   end
 
   it "converts XML to ISO STS" do
-    Dir.mktmpdir do |dir|
-      sts_path = File.join(dir, "rice-en.cd.isosts.xml")
+    sts_path = File.join(Dir.pwd, "rice-en.cd.isosts.xml")
 
-      FileUtils.rm_f(sts_path)
-      MnConvert.convert(
-        mn_xml, sts_path,
-        { input_format: :metanorma, output_format: :iso, debug: true }
-      )
+    FileUtils.rm_f(sts_path)
+    MnConvert.convert(
+      mn_xml, sts_path,
+      { input_format: :metanorma, output_format: :iso, debug: true }
+    )
 
-      expect(File.exist?(sts_path)).to be true
-      expect(File.read(sts_path)).to include '<preformat preformat-type="ruby">'
-    end
+    expect(File.exist?(sts_path)).to be true
+    expect(File.read(sts_path)).to include '<preformat preformat-type="ruby">'
   end
 
   %w(adoc xml).each do |format|
     it "converts STS to MN #{format} in specified location" do
-      Dir.mktmpdir do |dir|
-        sts_path = ->(fmt) { File.join(dir, "sts.#{fmt}") }
-        mn_path = ->(fmt) { File.join(dir, "mn.#{fmt}") }
+      sts_path = ->(fmt) { File.join(Dir.pwd, "sts.#{fmt}") }
+      mn_path = ->(fmt) { File.join(Dir.pwd, "mn.#{fmt}") }
 
-        source = sts_path.("xml")
-        mn_dest = mn_path.(format)
-        FileUtils.cp(sts_xml, source)
+      source = sts_path.("xml")
+      mn_dest = mn_path.(format)
+      FileUtils.cp(sts_xml, source)
 
-        MnConvert.convert(
-          source, mn_dest,
-          { input_format: :sts,
-            output_format: format,
-            debug: true }
-        )
+      MnConvert.convert(
+        source, mn_dest,
+        { input_format: :sts,
+          output_format: format,
+          debug: true }
+      )
 
-        expect(File.exist?(mn_dest)).to be true
-      end
+      expect(File.exist?(mn_dest)).to be true
     end
   end
 
   it "converts STS to MN adoc by default" do
-    Dir.mktmpdir do |dir|
-      sts_path = ->(fmt) { File.join(dir, "sts.#{fmt}") }
+    sts_path = ->(fmt) { File.join(Dir.pwd, "sts.#{fmt}") }
 
-      source = sts_path.("xml")
-      mn_adoc = sts_path.("adoc")
-      FileUtils.cp(sts_xml, source)
-      MnConvert.convert(
-        source, mn_adoc,
-        { input_format: MnConvert::InputFormat::STS }
-      )
+    source = sts_path.("xml")
+    mn_adoc = sts_path.("adoc")
+    FileUtils.cp(sts_xml, source)
+    MnConvert.convert(
+      source, mn_adoc,
+      { input_format: MnConvert::InputFormat::STS }
+    )
 
-      expect(File.exist?(mn_adoc)).to be true
-    end
+    expect(File.exist?(mn_adoc)).to be true
   end
 
   it "splits bibdata and generates adoc output" do
-    mn_path = ->(dir, fmt) { File.join(dir, "sts.#{fmt}") }
-    Dir.mktmpdir do |dir|
-      FileUtils.cp(sts_xml, mn_path.(dir, "xml"))
-      MnConvert.convert(
-        mn_path.(dir, "xml"),
-        mn_path.(dir, "adoc"),
-        { input_format: MnConvert::InputFormat::STS,
-          split_bibdata: true,
-          debug: true },
-      )
+    mn_path = ->(fmt) { File.join(Dir.pwd, "sts.#{fmt}") }
+    FileUtils.cp(sts_xml, mn_path.("xml"))
+    MnConvert.convert(
+      mn_path.("xml"),
+      mn_path.("adoc"),
+      { input_format: MnConvert::InputFormat::STS,
+        split_bibdata: true,
+        debug: true },
+    )
 
-      expect(File.exist?(mn_path.(dir, "adoc"))).to be true
-      expect(File.exist?(mn_path.(dir, "rxl"))).to be true
-    end
+    expect(File.exist?(mn_path.("adoc"))).to be true
+    expect(File.exist?(mn_path.("rxl"))).to be true
   end
 
   let(:sts_xml) do
